0a4e1ba44c5f7479518ee03688156900f5aa7bd1
Muhammad Hussain <muhammadhussain2644@gmail.com>
2025-03-01 17:34:20 +0500

    fix bug

--- src/pages/verify_email/Verify_email.jsx
+++ src/pages/verify_email/Verify_email.jsx
@@ -16,7 +16,7 @@ const Verify_email = () => {
 
       try {
         const res = await fetch(
-          `http://localhost:5000/api/auth/verify-email?token=${token}`
+          `http://localhost:5000/api/v1/verify-email?token=${token}`
         );
         const data = await res.json();
         alert(data.message);
