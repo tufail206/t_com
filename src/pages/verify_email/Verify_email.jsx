6ddef951b1269e2a54aaef766962f89abf30f4fe
Muhammad Hussain <muhammadhussain2644@gmail.com>
2025-03-01 17:28:30 +0500

    make verify email page design

--- src/pages/verify_email/Verify_email.jsx
+++ src/pages/verify_email/Verify_email.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Verify_email = () => {
-  return (
-    <div>index</div>
-  )
-}
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verifyEmail = async () => {
+      const params = new URLSearchParams(window.location.search);
+      const token = params.get("token");
+
+      if (!token) {
+        alert("Invalid verification link");
+        return;
+      }
+
+      try {
+        const res = await fetch(
+          `http://localhost:5000/api/auth/verify-email?token=${token}`
+        );
+        const data = await res.json();
+        alert(data.message);
+        navigate("/login");
+      } catch (error) {
+        console.error("Verification error", error);
+      }
+    };
+
+    verifyEmail();
+  }, [navigate]);
+
+  return <h2>Verifying Email...</h2>;
+};
+
+
 
 export default Verify_email;
